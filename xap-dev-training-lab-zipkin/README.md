0f68904072d39af86d85bc78cf8e0ee39a8c7f0e
aharonmoll <aharon@gigaspaces.com>
2020-08-13 15:44:50 +0300

    Add new Distributed Tracing (zipkin) lab

---
+++ xap-dev-training-lab-zipkin/README.md
@@ -0,0 +1,53 @@
+# xap-dev-training - lab-zipkin
+
+## 	Distributed Tracing (Zipkin)
+
+###### Lab Goals
+1.  Get familiar with Distributed Tracing (zipkin) to be able to improve performance and code efficiency.
+2.  Use code with Distributed Tracing activated
+3. Install and setup zipkin.
+
+###### Lab Description
+This lab includes the needed instructions and code for activate and use zipkin (Distributed Tracing)
+
+
+## 1 Lab setup
+Make sure you restart gs-agent and gs-ui (or at least undeploy all Processing Units using gs-ui)
+
+**1.1** Open %XAP_TRAINING_HOME%/xap-dev-training-lab-zipkin project with intellij (open pom.xml)<br>
+**1.2** Run mvn install
+
+    ~/xap-dev-training/xap-dev-training-lab-zipkin$ mvn install
+    
+    [INFO] --- maven-install-plugin:2.4:install (default-install) @ zipkin ---
+    [INFO] Installing ~/xap-dev-training/xap-dev-training-lab-zipkin/target/zipkin-1.0-SNAPSHOT.jar to ~/.m2/repository/zipkin/zipkin/1.0-SNAPSHOT/zipkin-1.0-SNAPSHOT.jar
+    [INFO] Installing ~/xap-dev-training/xap-dev-training-lab-zipkin/pom.xml to ~/.m2/repository/zipkin/zipkin/1.0-SNAPSHOT/zipkin-1.0-SNAPSHOT.pom
+    [INFO] ------------------------------------------------------------------------
+    [INFO] BUILD SUCCESS
+    [INFO] ----------------------------------------------------------------------
+
+
+
+## 2	Install and setup zipkin
+**2.1**	`curl -sSL https://zipkin.io/quickstart.sh | bash -s` <br />
+**2.2**	`java -jar zipkin.jar` <br />
+
+![snapshot](Pictures/Picture1.png)
+
+## 3 Perform GS operations when zipkin is on
+
+**3.1**	Expand src -> main -> java -> com.gs.test
+and investigate the code of the TraceHelper class <br />
+**3.2**	Run TraceHelper main:
+* Using IntelliJ:
+
+![snapshot](Pictures/Picture2.png)
+
+* Using maven cli:<br>
+`cd ~/xap-dev-training/xap-dev-training-lab-zipkin`<br>
+`mvn exec:java  -D"exec.mainClass"="com.gs.test.TraceHelper" -Dexec.classpathScope=compile -Dcom.gs.jini_lus.locators=localhost`<br>
+
+## 4 Login to zipkin console and verify the results
+**4.1**	click on http://localhost:9411<br>
+**4.2**	Explore recent Trace IDs:<br>
+![snapshot](Pictures/Picture3.png)
---
+++ xap-dev-training-lab-zipkin/src/main/java/com/gs/test/TraceHelper.java
@@ -0,0 +1,126 @@
+package com.gs.test;
+import com.gigaspaces.annotation.pojo.SpaceId;
+import com.gigaspaces.annotation.pojo.SpaceRouting;
+import com.gigaspaces.tracing.ZipkinTracerBean;
+import io.opentracing.Scope;
+import io.opentracing.Tracer;
+import io.opentracing.util.GlobalTracer;
+import org.openspaces.core.GigaSpace;
+import org.openspaces.core.GigaSpaceConfigurer;
+import org.openspaces.core.space.SpaceProxyConfigurer;
+
+import java.util.concurrent.Callable;
+
+    public class TraceHelper {
+    private static ZipkinTracerBean tracingBean;
+    private static TraceHelper helper;
+    public static String zipkinurl = "http://localhost:9411/";
+
+
+    protected TraceHelper() {
+        tracingBean = new ZipkinTracerBean("test").setStartActive(true).setZipkinUrl(zipkinurl);
+        tracingBean.start();
+    }
+
+    static public TraceHelper getInstance() {
+        if (helper != null)
+            return helper;
+        helper = new TraceHelper();
+        return helper;
+    }
+
+    public static <T> T wrap(String name, Callable<T> c) throws Exception {
+        if (GlobalTracer.isRegistered()) {
+            Tracer tracer = GlobalTracer.get();
+            io.opentracing.Span span = tracer.buildSpan(name).start();
+            //noinspection unused
+            try (Scope scope = tracer.scopeManager().activate(span)) {
+                return c.call();
+            } catch (Exception e) {
+                span.log(e.toString());
+                throw e;
+            } finally {
+                // Optionally finish the Span if the operation it represents
+                // is logically completed at this point.
+                span.finish();
+            }
+        } else {
+            return c.call();
+        }
+    }
+
+    /**
+     * Test
+     * @param args
+     */
+    static GigaSpace gs;
+    public static void main(String[] args) throws Exception {
+        TraceHelper helper = TraceHelper.getInstance();
+        Tracer tracer = GlobalTracer.get();
+        gs = new GigaSpaceConfigurer(new SpaceProxyConfigurer("demo").lookupGroups("xap-15.5.0")).create();
+        wrap("write", () -> {
+            write();
+            return 0;
+        });
+
+        wrap("read", () -> {
+            read();
+            return 0;
+        });
+
+        helper.close();
+    }
+
+    public static void read() {
+        gs.read(new MyTest());
+    }
+
+    public static void write() {
+        gs.write(new MyTest("3","myTest"));
+    }
+
+    public static void close(){
+        if (tracingBean != null){
+            try {
+                tracingBean.destroy();
+            }
+            catch (Exception e){}
+        }
+    }
+
+    public static class MyTest implements java.io.Serializable{
+        String id;
+        String txt;
+
+        public MyTest() {
+        }
+
+        public MyTest(String id, String txt) {
+            this.id = id;
+            this.txt = txt;
+        }
+
+        @SpaceId
+        @SpaceRouting
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+
+        public String getTxt() {
+            return txt;
+        }
+
+        public void setTxt(String txt) {
+            this.txt = txt;
+        }
+    }
+
+
+
+}
+
