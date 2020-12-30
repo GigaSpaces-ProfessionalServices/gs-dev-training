cb01daeb2dc1296c399c38c3caa6520f04114048
aharonmoll <aharon@gigaspaces.com>
2020-12-30 13:05:21 +0200

    Update change c123 to gs script

--- Change_c123_to_gs.sh
+++ Change_c123_to_gs.sh
@@ -11,3 +11,8 @@ mv $1/BillBuddyWebApplication/src/main/java/com/c123 $1/BillBuddyWebApplication/
 mv $1/BillBuddy_Space/src/main/java/com/c123 $1/BillBuddy_Space/src/main/java/com/gs
 
 mv $1/BillBuddyPersistency/src/main/java/com/c123 $1/BillBuddyPersistency/src/main/java/com/gs
+
+#AfterDone the above from the intellij:
+#1. Replace .c123. to .gs.
+#2. Replace 123Completed to gsUniversity
+#3. Add new directories created and commit and push.
