8c5c21aca3e53f0d94b3cf01402fad1fbb722239
aharonmoll <aharon@gigaspaces.com>
2020-12-30 15:57:53 +0200

    Change c123 to gs in Lab12 exercise

--- Change_c123_to_gs.sh
+++ Change_c123_to_gs.sh
@@ -16,6 +16,8 @@ mv $1/merchantsOfTop5Payments/src/main/java/com/c123 $1/merchantsOfTop5Payments/
 
 mv $1/BillBuddyMultiplePaymentFeeder/src/main/java/com/c123 $1/BillBuddyMultiplePaymentFeeder/src/main/java/com/gs
 
+mv $1/BillBuddyAggregationPayment/src/main/java/com/c123 $1/BillBuddyAggregationPayment/src/main/java/com/gs
+
 #AfterDone the above from the intellij:
 #1. Replace .c123. to .gs.
 #2. Replace 123Completed to gsUniversity
