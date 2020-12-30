ba3928c259a22a6c3b31aa5299c1a82f8f4cd899
aharonmoll <aharon@gigaspaces.com>
2020-12-30 18:29:38 +0200

    Change c123 to gs Lab17 solution

---
+++ xap-dev-training-lab17-solution/BillBuddyModel/src/main/java/com/gs/billbuddy/remoting/IMerchantProfitService.java
@@ -0,0 +1,20 @@
+package com.gs.billbuddy.remoting;
+
+import org.openspaces.remoting.Routing;
+
+
+/** 
+* IMerchantProfitService Interface. 
+*  
+* Define method which will be executed by remoting on top of the space
+* 
+* @author gsUniversity
+*/
+
+public interface IMerchantProfitService{
+  
+	Double getMerchantProfit(@Routing Integer merchantAccountId);
+
+	// Other option for Routing on remoting call
+	//	Double getMerchantProfit(@Routing Integer merchantAccountId);
+}
